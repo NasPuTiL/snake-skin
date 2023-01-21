7281e492787806f89c6b6a1948bb06c355cc9be4
NasPuTiL <nasputil@gmail.com>
2023-01-21 20:20:15 +0100

    Init commit

---
+++ backend/skin-snke/src/main/java/com/skin/snake/skinsnke/SkinSnkeApplication.java
@@ -0,0 +1,13 @@
+package com.skin.snake.skinsnke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkinSnkeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkinSnkeApplication.class, args);
+	}
+
+}
---
+++ backend/skin-snke/src/test/java/com/skin/snake/skinsnke/SkinSnkeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skin.snake.skinsnke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkinSnkeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

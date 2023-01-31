9de5f139c048f333cf53e3b28470baac182fae8c
NasPuTiL <nasputil@gmail.com>
2023-01-31 21:25:08 +0100

    SNAKE-0000 authentication

--- src/main/java/com/educate/skinsnake/api/user/AuthController.java
+++ src/main/java/com/educate/skinsnake/api/user/AuthController.java
@@ -6,9 +6,12 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 
 @RestController
 @AllArgsConstructor
@@ -22,8 +25,22 @@ public class AuthController {
             @ApiResponse(code = 404, message = "Not found - The product was not found"),
             @ApiResponse(code = 500, message = "Server error")
     })
-    @PostMapping(value = "/login")
-    public String login(@RequestBody Authentication authentication) {
-        return tokenService.generateToken(authentication);
+    @PostMapping(value = "/token")
+    public String generateToken(Authentication authentication) {
+        String s = tokenService.generateToken(authentication);
+        System.out.println("s = " + s);
+        return s;
+    }
+
+
+    @ApiOperation(value = "User login endpoint")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved"),
+            @ApiResponse(code = 404, message = "Not found - The product was not found"),
+            @ApiResponse(code = 500, message = "Server error")
+    })
+    @GetMapping
+    public String test(Principal principal) {
+        return "Hello " + principal.getName() + "!";
     }
 }
--- src/main/java/com/educate/skinsnake/config/SecurityConfig.java
+++ src/main/java/com/educate/skinsnake/config/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.educate.skinsnake.config;
 
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +15,13 @@ import org.springframework.security.config.annotation.web.configurers.oauth2.ser
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
-
-;
 
 @Configuration
 @EnableWebSecurity
@@ -38,15 +44,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .authorizeRequests(auth -> auth.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(withDefaults())
                 .build();
     }
 
     @Bean
-    public JwtDecoder jwtDecoder() {
+    JwtDecoder jwtDecoder() {
         return NimbusJwtDecoder.withPublicKey(rsaKeys.getPublicKey()).build();
+    }
+
+    @Bean
+    JwtEncoder jwtEncoder() {
+        JWK jwk = new RSAKey.Builder(rsaKeys.getPublicKey()).privateKey(rsaKeys.getPrivateKey()).build();
+        JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
+        return new NimbusJwtEncoder(jwks);
     }
 }
